c4e7776e6d2a2b108aa1405dab555159d102ec78
aabeborn <aabeborn@benato.dev>
2025-02-15 19:17:43 +0100

    🏷️ Update text component types

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,23 +1,21 @@
 import { FC, HTMLAttributes, useMemo } from 'react'
 import './index.css'
+import clsx from 'clsx'
 
+type Variant = 'body-1' | 'title-1'
 interface Props extends HTMLAttributes<HTMLElement> {
 	as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'div'
-	variant?: 'body-1' | 'title-1'
+	variant?: Variant
 }
 
-const variantsClasses: Record<NonNullable<Props['variant']>, string> = {
+const variantsClasses: Record<Variant, string> = {
 	'body-1': 'text-body-1',
 	'title-1': 'text-title-1'
 }
 
 const Text: FC<Props> = ({ as = 'span', children, className, variant = 'body-1', ...props }) => {
 	const Component = as
-
-	const classes = useMemo(() => {
-		return `${variantsClasses[variant]} ${className ?? ''}`
-	}, [])
-
+	const classes = useMemo(() => clsx(variantsClasses[variant], className), [variant, className])
 	return (
 		<Component
 			className={classes}
